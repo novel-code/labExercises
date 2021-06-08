167871a93a12fd1e96a399278d11fde5db552163
syed <novelcode0@gmail.com>
2021-06-08 10:35:49 +0530

    first commit

---
+++ week1/src/AddTwoNumbers.java
@@ -0,0 +1,8 @@
+
+public class AddTwoNumbers {
+	public static void main(String[] args) {
+		int number1 = 200, number2 = 50, sum;
+		sum = number1 + number2;
+		System.out.println("Sum of these numbers:" +sum);
+	}
+}
